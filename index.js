5254a3fedaae6a572775fa8413cdd5d76f170f53
Andre <andreferreira@live.co.uk>
2021-07-05 10:42:20 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// const express = require("express");
+// const path = require("path");
+
+// const app = express();
+
+// app.get("/", (req, res) => {
+//   res.send("HEllo");
+// });
+
+// app.listen("3000", () => {
+//   console.log("LOGGED");
+// });
